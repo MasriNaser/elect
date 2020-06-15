cbfd3fc4e7b668fe2bac59d681693cdec91f3701
MasriNaser <Naser.Masri@cronos.be>
2020-06-15 10:42:36 +0200

    condition

---
+++ if.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int x;
+  // cin >> x;
+  x = 51;
+  if (x % 2 == 0)
+  {
+    cout << "this num is even";
+  }else
+  {
+    cout << "NO!";
+  }
+}
