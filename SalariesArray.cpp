99291f044a7b1c6ffcbb7f9ba993a5f4a9ed9ec8
MasriNaser <Naser.Masri@cronos.be>
2020-06-18 17:58:49 +0200

    av sal

--- SalariesArray.cpp
+++ SalariesArray.cpp
@@ -2,14 +2,23 @@
 using namespace std;
 int main(int argc, const char **argv)
 {
-  const int size = 7;
-  int list[size];
+  const int size = 3;
+  int salary[size];
+  float sum = 0.0;
   for (int i = 0; i < size; i++)
   {
-    cout << "Entr Item value\n";
-    cin >> list[i];
+    cout << "Entr salary number:" << i + 1 << "\n";
+    cin >> salary[i];
+  }
+  for (int j = 0; j <= 3; j++)
+  {
+    salary[j] += salary[j] * 2;
+  }
+  for (int x = 0; x < 3; x++)
+  {
+    sum += salary[x];
   }
 
-  cout << size;
+  cout << " The Av salis: " << sum / size << "\n";
   return 0;
 }
